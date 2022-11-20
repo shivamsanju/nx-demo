1c4df479eb0a4bbad2673afc6b59935b91418677
Shivam Sanju <shvmsanju@gmail.com>
2022-11-20 16:35:53 +0530

    setup nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,28 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import allowSingleSession from 'nx';
 
 export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
-  debug: true,
-}
+
+  // add callbacks
+  callbacks: {
+    async jwt({ token, account }: { token: any; account?: any }) {
+      token = allowSingleSession(token, account, 10000);
+      return token;
+    },
+
+    async session({ session, token }: { session: any; token: any }) {
+      session.authenticated = token.authenticated;
+      return session;
+    },
+  },
+};
+
 export default NextAuth(authOptions);
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,32 +3,10 @@ import styles from '../styles/Home.module.css';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (session) {
+  const { data: session }: { data: any } = useSession();
+
+  if (session && session.authenticated) {
     return (
-      <>
-        <div className={styles.container}>
-          <Head>
-            <title>NX Demo</title>
-            <meta name='description' content='NX Demo' />
-            <link rel='icon' href='/favicon.ico' />
-          </Head>
-
-          <main className={styles.main}>
-            <h1 className={styles.title}>
-              Signed in as <span>{session?.user?.name}</span>
-            </h1>
-            <button className={styles.button} onClick={() => signOut()}>
-              Sign out
-            </button>
-          </main>
-        </div>
-      </>
-    );
-  }
-
-  return (
-    <>
       <div className={styles.container}>
         <Head>
           <title>NX Demo</title>
@@ -37,12 +15,31 @@ export default function Home() {
         </Head>
 
         <main className={styles.main}>
-          <h1 className={styles.title}>Not signed in</h1>
-          <button className={styles.button} onClick={() => signIn()}>
-            Sign in
+          <h1 className={styles.title}>
+            Signed in as <span>{session?.user?.name}</span>
+          </h1>
+          <button className={styles.button} onClick={() => signOut()}>
+            Sign out
           </button>
         </main>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>NX Demo</title>
+        <meta name='description' content='NX Demo' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Not signed in</h1>
+        <button className={styles.button} onClick={() => signIn()}>
+          Sign in
+        </button>
+      </main>
+    </div>
   );
 }
