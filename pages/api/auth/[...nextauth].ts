c1dbd59c3231153362db73f8c61ac77e6be975af
Shivam Sanju <shvmsanju@gmail.com>
2022-11-23 19:34:17 +0530

    client_id & client_secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.CLIENT_ID || '',
+      clientSecret: process.env.CLIENT_SECRET || '',
     }),
   ],
 
